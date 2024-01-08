8e4e35b06f585f6e927fa732c11058e560ee3fe9
efjerb <eusb@live.dk>
2024-01-08 15:10:30 +0100

    Create method to instantiate ports

--- RevitToRDFConverter/RelatedPorts.cs
+++ RevitToRDFConverter/RelatedPorts.cs
@@ -87,7 +87,7 @@ namespace RevitToRDFConverter
 
                     }
 
-                    if (connector.Shape.ToString() == "Rectangular")
+                    else if (connector.Shape.ToString() == "Rectangular")
                     {
                         connectorWidthID = System.Guid.NewGuid().ToString().Replace(' ', '-');
                         connectorHeightID = System.Guid.NewGuid().ToString().Replace(' ', '-');
@@ -438,104 +438,11 @@ namespace RevitToRDFConverter
                 
                 if (Domain.DomainHvac == connector.Domain || Domain.DomainPiping == connector.Domain)
                 {
-                    //Type
+                    // Create the connector ID
                     connectorID = componentID + "-" + connector.Id;
-                    sb.Append($"inst:{componentID} fso:hasPort inst:{connectorID} ." + "\n"
-                        + $"inst:{connectorID} a fso:Port ." + "\n");
-
-
-                    //FlowDirection
-                    connectorDirectionID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                    connectorDirection = connector.Direction.ToString();
-                    connectorDirectionVectorZID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                    connectorDirectionVectorZ = connector.CoordinateSystem.BasisZ.ToString();
-
-                    sb.Append($"inst:{connectorID} fpo:hasFlowDirection inst:{connectorDirectionID} ." + "\n"
-                                            + $"inst:{connectorDirectionID} a fpo:FlowDirection ." + "\n"
-                                            + $"inst:{connectorDirectionID} fpo:hasValue '{connectorDirection}'^^xsd:string ." + "\n"
-                                            + $"inst:{connectorID} ex:hasFlowDirectionVectorZ inst:{connectorDirectionVectorZID} ." + "\n"
-                                            + $"inst:{connectorDirectionVectorZID} fpo:hasValue '{connectorDirectionVectorZ}'^^xsd:string ." + "\n"
-                                            );
-
-                    //Size
-                    if (connector.Shape.ToString() == "Round")
-                    {
-                        connectorOuterDiameterID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                        connectorOuterDiameter = UnitUtils.ConvertFromInternalUnits(connector.Radius * 2, UnitTypeId.Meters);
-                        sb.Append($"inst:{connectorID} fpo:hasOuterDiameter inst:{connectorOuterDiameterID} ." + "\n" +
-                            $"inst:{connectorOuterDiameterID} a fpo:OuterDiameter ." + "\n" +
-                            $"inst:{connectorOuterDiameterID} fpo:hasValue '{connectorOuterDiameter}'^^xsd:double ." + "\n" +
-                            $"inst:{connectorOuterDiameterID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
-
-                        crossSectionalAreaID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                        crossSectionalArea = Math.PI * Math.Pow(UnitUtils.ConvertFromInternalUnits(connector.Radius, UnitTypeId.Meters),2);
-                        sb.Append($"inst:{connectorID} fpo:hasCrossSectionalArea inst:{crossSectionalAreaID} ." + "\n" +
-                            $"inst:{crossSectionalAreaID} a fpo:CrossSectionalArea ." + "\n" +
-                            $"inst:{crossSectionalAreaID} fpo:hasValue '{crossSectionalArea}'^^xsd:double ." + "\n" +
-                            $"inst:{crossSectionalAreaID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
-
-                    }
-
-                    else if (connector.Shape.ToString() == "Rectangular")
-                    {
-                        connectorWidthID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                        connectorHeightID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                        connectorHeight = UnitUtils.ConvertFromInternalUnits(connector.Height, UnitTypeId.Meters);
-                        connectorWidth = UnitUtils.ConvertFromInternalUnits(connector.Width, UnitTypeId.Meters);
-                        sb.Append($"inst:{connectorID} fpo:hasHeight inst:{connectorHeightID} ." + "\n" +
-                            $"inst:{connectorHeightID} a fpo:Heigth ." + "\n" +
-                            $"inst:{connectorHeightID} fpo:hasValue '{connectorHeight}'^^xsd:double ." + "\n" +
-                            $"inst:{connectorHeightID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n" +
-                            $"inst:{connectorID} fpo:hasWidth inst:{connectorWidthID} ." + "\n" +
-                            $"inst:{connectorWidthID} a fpo:Width ." + "\n" +
-                            $"inst:{connectorWidthID} fpo:hasValue '{connectorWidth}'^^xsd:double ." + "\n" +
-                            $"inst:{connectorWidthID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
-
-                        crossSectionalAreaID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                        crossSectionalArea = connectorHeight * connectorWidth;
-                        sb.Append($"inst:{connectorID} fpo:hasCrossSectionalArea inst:{crossSectionalAreaID} ." + "\n" +
-                            $"inst:{crossSectionalAreaID} a fpo:CrossSectionalArea ." + "\n" +
-                            $"inst:{crossSectionalAreaID} fpo:hasValue '{crossSectionalArea}'^^xsd:double ." + "\n" +
-                            $"inst:{crossSectionalAreaID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
-
-                        
-
-                    }
-                    //if (connector.Flow != null)
-                    //{
-                    //    //Flow rate
-                    //    string flowID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                    //    double flowValue = UnitUtils.ConvertFromInternalUnits(connector.Flow, UnitTypeId.LitersPerSecond);
-                    //    sb.Append($"inst:{connectorID} fpo:hasFlowRate inst:{flowID} ." + "\n"
-                    //     + $"inst:{flowID} a fpo:FlowRate ." + "\n"
-                    //     + $"inst:{flowID} fpo:hasValue '{flowValue}'^^xsd:double ." + "\n"
-                    //     + $"inst:{flowID} fpo:hasUnit 'Liters per second'^^xsd:string ." + "\n");
-                    //}
                     
-                    if (connectorDirection == "Out")
-                    {
-                        if (component.LookupParameter("Reynolds number") != null)
-                        {
-                            //Reynolds number
-                            string reynoldsID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                            double reynoldsValue = component.LookupParameter("Reynolds number").AsDouble();
-                            sb.Append($"inst:{connectorID} fpo:hasReynoldsNumber inst:{reynoldsID} ." + "\n"
-                             + $"inst:{reynoldsID} a fpo:ReynoldsNumber ." + "\n"
-                             + $"inst:{reynoldsID} fpo:hasValue '{reynoldsValue}'^^xsd:double ." + "\n");
-                        }
-
-
-                        if (component.LookupParameter("Flow State") != null)
-                        {
-                            //Flow State
-                            string flowStateID = System.Guid.NewGuid().ToString().Replace(' ', '-');
-                            string flowStateValue = component.LookupParameter("Flow State").AsValueString();
-                            sb.Append($"inst:{connectorID} fpo:hasFlowState inst:{flowStateID} ." + "\n"
-                             + $"inst:{flowStateID} a fpo:FlowState ." + "\n"
-                             + $"inst:{flowStateID} fpo:hasValue '{flowStateValue}'^^xsd:string ." + "\n"
-                             + $"inst:{flowStateID} fpo:hasUnit 'Pascal'^^xsd:string ." + "\n");
-                        }
-                    }
+                    // Instantiate the port
+                    sb.Append(CreateConnector(component, connector, componentID));
 
                     //Port relationship to other ports
                     sb.Append(JoinConnectors(connector, connectorID, componentID));
@@ -546,9 +453,105 @@ namespace RevitToRDFConverter
             return sb;
         }
 
-                    }
+        public static StringBuilder CreateConnector(Element component, Connector connector, string componentID)
+        {
+            StringBuilder sb = new StringBuilder();
 
-                    
+            //Type
+            string connectorID = componentID + "-" + connector.Id;
+            sb.Append($"inst:{componentID} fso:hasPort inst:{connectorID} ." + "\n"
+                + $"inst:{connectorID} a fso:Port ." + "\n");
+
+
+            //FlowDirection
+            string connectorDirectionID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+            string connectorDirection = connector.Direction.ToString();
+            string connectorDirectionVectorZID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+            string connectorDirectionVectorZ = connector.CoordinateSystem.BasisZ.ToString();
+
+            sb.Append($"inst:{connectorID} fpo:hasFlowDirection inst:{connectorDirectionID} ." + "\n"
+                                    + $"inst:{connectorDirectionID} a fpo:FlowDirection ." + "\n"
+                                    + $"inst:{connectorDirectionID} fpo:hasValue '{connectorDirection}'^^xsd:string ." + "\n"
+                                    + $"inst:{connectorID} ex:hasFlowDirectionVectorZ inst:{connectorDirectionVectorZID} ." + "\n"
+                                    + $"inst:{connectorDirectionVectorZID} fpo:hasValue '{connectorDirectionVectorZ}'^^xsd:string ." + "\n"
+                                    );
+
+            //Size
+            if (connector.Shape.ToString() == "Round")
+            {
+                string connectorOuterDiameterID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                double connectorOuterDiameter = UnitUtils.ConvertFromInternalUnits(connector.Radius * 2, UnitTypeId.Meters);
+                sb.Append($"inst:{connectorID} fpo:hasOuterDiameter inst:{connectorOuterDiameterID} ." + "\n" +
+                    $"inst:{connectorOuterDiameterID} a fpo:OuterDiameter ." + "\n" +
+                    $"inst:{connectorOuterDiameterID} fpo:hasValue '{connectorOuterDiameter}'^^xsd:double ." + "\n" +
+                    $"inst:{connectorOuterDiameterID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
+
+                string crossSectionalAreaID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                double crossSectionalArea = Math.PI * Math.Pow(UnitUtils.ConvertFromInternalUnits(connector.Radius, UnitTypeId.Meters), 2);
+                sb.Append($"inst:{connectorID} fpo:hasCrossSectionalArea inst:{crossSectionalAreaID} ." + "\n" +
+                    $"inst:{crossSectionalAreaID} a fpo:CrossSectionalArea ." + "\n" +
+                    $"inst:{crossSectionalAreaID} fpo:hasValue '{crossSectionalArea}'^^xsd:double ." + "\n" +
+                    $"inst:{crossSectionalAreaID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
+
+            }
+
+            else if (connector.Shape.ToString() == "Rectangular")
+            {
+                string connectorWidthID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                string connectorHeightID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                double connectorHeight = UnitUtils.ConvertFromInternalUnits(connector.Height, UnitTypeId.Meters);
+                double connectorWidth = UnitUtils.ConvertFromInternalUnits(connector.Width, UnitTypeId.Meters);
+                sb.Append($"inst:{connectorID} fpo:hasHeight inst:{connectorHeightID} ." + "\n" +
+                    $"inst:{connectorHeightID} a fpo:Heigth ." + "\n" +
+                    $"inst:{connectorHeightID} fpo:hasValue '{connectorHeight}'^^xsd:double ." + "\n" +
+                    $"inst:{connectorHeightID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n" +
+                    $"inst:{connectorID} fpo:hasWidth inst:{connectorWidthID} ." + "\n" +
+                    $"inst:{connectorWidthID} a fpo:Width ." + "\n" +
+                    $"inst:{connectorWidthID} fpo:hasValue '{connectorWidth}'^^xsd:double ." + "\n" +
+                    $"inst:{connectorWidthID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
+
+                string crossSectionalAreaID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                double crossSectionalArea = connectorHeight * connectorWidth;
+                sb.Append($"inst:{connectorID} fpo:hasCrossSectionalArea inst:{crossSectionalAreaID} ." + "\n" +
+                    $"inst:{crossSectionalAreaID} a fpo:CrossSectionalArea ." + "\n" +
+                    $"inst:{crossSectionalAreaID} fpo:hasValue '{crossSectionalArea}'^^xsd:double ." + "\n" +
+                    $"inst:{crossSectionalAreaID} fpo:hasUnit 'Meter'^^xsd:string  ." + "\n");
+
+                
+            }
+            //if (connector.Flow != null)
+            //{
+            //    //Flow rate
+            //    string flowID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+            //    double flowValue = UnitUtils.ConvertFromInternalUnits(connector.Flow, UnitTypeId.LitersPerSecond);
+            //    sb.Append($"inst:{connectorID} fpo:hasFlowRate inst:{flowID} ." + "\n"
+            //     + $"inst:{flowID} a fpo:FlowRate ." + "\n"
+            //     + $"inst:{flowID} fpo:hasValue '{flowValue}'^^xsd:double ." + "\n"
+            //     + $"inst:{flowID} fpo:hasUnit 'Liters per second'^^xsd:string ." + "\n");
+            //}
+
+            if (connectorDirection == "Out")
+            {
+                if (component.LookupParameter("Reynolds number") != null)
+                {
+                    //Reynolds number
+                    string reynoldsID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                    double reynoldsValue = component.LookupParameter("Reynolds number").AsDouble();
+                    sb.Append($"inst:{connectorID} fpo:hasReynoldsNumber inst:{reynoldsID} ." + "\n"
+                     + $"inst:{reynoldsID} a fpo:ReynoldsNumber ." + "\n"
+                     + $"inst:{reynoldsID} fpo:hasValue '{reynoldsValue}'^^xsd:double ." + "\n");
+                }
+
+
+                if (component.LookupParameter("Flow State") != null)
+                {
+                    //Flow State
+                    string flowStateID = System.Guid.NewGuid().ToString().Replace(' ', '-');
+                    string flowStateValue = component.LookupParameter("Flow State").AsValueString();
+                    sb.Append($"inst:{connectorID} fpo:hasFlowState inst:{flowStateID} ." + "\n"
+                     + $"inst:{flowStateID} a fpo:FlowState ." + "\n"
+                     + $"inst:{flowStateID} fpo:hasValue '{flowStateValue}'^^xsd:string ." + "\n"
+                     + $"inst:{flowStateID} fpo:hasUnit 'Pascal'^^xsd:string ." + "\n");
                 }
             }
 
@@ -578,23 +581,23 @@ namespace RevitToRDFConverter
         }
 
         public static StringBuilder InstantiateJoin(Connector connector, Connector connectedConnector, string connectorID, string componentID)
-                        {
+        {
             StringBuilder sb = new StringBuilder();
-
+            
             (string portPredicate, string componentPredicate) = GetPredicates(connector, connectedConnector);
 
             string connectedConnectorID = connectedConnector.Owner.UniqueId.ToString() + "-" + connectedConnector.Id.ToString();
             string connectedComponentID = connectedConnector.Owner.UniqueId.ToString();
 
             if (Domain.DomainHvac == connectedConnector.Domain || Domain.DomainPiping == connectedConnector.Domain)
-                        {
+            {
                 sb.Append($"inst:{connectorID} fso:suppliesFluidTo inst:{connectedConnectorID} ." + "\n"
-                                + $"inst:{connectedConnectorID} a fso:Port ." + "\n"
-                                + $"inst:{componentID} fso:feedsFluidTo inst:{connectedComponentID} ." + "\n"
-);
-                        }
+                    + $"inst:{connectedConnectorID} a fso:Port ." + "\n"
+                    + $"inst:{componentID} fso:feedsFluidTo inst:{connectedComponentID} ." + "\n"
+                    );
+            }
             return sb;
-                    }
+        }
 
         public static (string portPredicate, string componentPredicate) GetPredicates(Connector connector, Connector connectedConnector)
         {
@@ -608,6 +611,6 @@ namespace RevitToRDFConverter
                 default:
                     return ("exchangesFluidWith", "exchangesFluidWith");
             }
+        }
     }
-}
 }
