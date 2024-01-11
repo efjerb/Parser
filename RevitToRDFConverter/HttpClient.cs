1ec3e087bb5a505f0486789b27c11b085fb8c9b4
efjerb <eusb@live.dk>
2024-01-11 08:17:55 +0100

    Add class for GraphDB requests

--- RevitToRDFConverter/HttpClient.cs
+++ RevitToRDFConverter/HttpClient.cs
@@ -3,12 +3,22 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace RevitToRDFConverter
 {
-
+    public class RepositoryResponse
+    {
+        public Dictionary<string, List<string>> head;
+        public Results results;
+    }
+    
+    public class Results
+    {
+        public List<Dictionary<string, Dictionary<string, string>>> bindings;
+    }
 
     public class HttpClientHelper
     {
@@ -27,6 +37,54 @@ namespace RevitToRDFConverter
         }
     }
 
+    public class GraphDBHTTPHelper
+    {
+        private static HttpClient client = new HttpClient();
+        public static bool PutData(string data)
+        {
+            string repositoryID = PromptGraphDBRepositoryID();
 
+            var data1 = new StringContent(data, Encoding.UTF8, "text/turtle");
+
+            var url = $"http://localhost:7200/repositories/{repositoryID}/statements";
+            HttpResponseMessage response = client.PutAsync(url, data1).Result;
+            
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                return true;
+            string returnValue = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Failed to POST data: ({response.StatusCode}): {returnValue}");
+
+        }
+        public static string PromptGraphDBRepositoryID()
+        {
+            List<string> repositoryList = GetReposirotyIDs();
+            GraphDBForm prompt = new GraphDBForm(repositoryList);
+            prompt.ShowDialog();
+            string repositoryID = prompt.repositoryId;
+
+            return repositoryID;
+        }
+        private static List<string> GetReposirotyIDs()
+        {
+            var url = $"http://localhost:7200/repositories";
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/sparql-results+json"));
+            
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string returnValue = response.Content.ReadAsStringAsync().Result;
+            //RepositoryResponse json = System.Text.Json.JsonSerializer.Deserialize<RepositoryResponse>(returnValue);
+            dynamic json = JsonConvert.DeserializeObject(returnValue);
+            List<string> repositoryIDs = new List<string>();
+
+            foreach (var item in json.results.bindings)
+            {
+                repositoryIDs.Add(item.id.value.ToString());
+            }
+            return repositoryIDs;
+        }
+    }
+
+
+     
 }
 
